a0855901c5a66768cfd9a1c7eb22f92476af76ca
Artiom Greenbuger <iwebchester@gmail.com>
2022-02-11 11:44:33 +0500

    [ADD] production server link

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,7 +2,6 @@ export const getApiHttpBaseUrl = () => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     return `${window.location.protocol}//${window.location.host}:${window.location.port}/api/`;
   } else {
-    //TODO: production url now throw error
-    throw new Error("Для работы с ПРОДАКШН сборкой установите адрес сервера");
+    return "https://feedback-task.vercel.app/";
   }
 };
